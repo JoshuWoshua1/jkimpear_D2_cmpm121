16aa95779f0e0cf6cdb6d344941e587f3923aa99
JoshuWoshua1 <jkimpear@ucsc.edu>
2025-10-15 16:44:07 +0000

    Step 1 complete: added canvas and edited style.css to give it a black border.
    Co-authored-by: Stan-21 <Stan-21@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 document.body.innerHTML = `
+  <h1>D2 assignment</h1>
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <canvas id ="myCanvas" width = "256" height = "256"></canvas>
 `;
